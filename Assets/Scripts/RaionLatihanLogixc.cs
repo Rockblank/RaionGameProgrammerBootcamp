423c9d2e5d959cbfe7cb6117d00ce5ea6f66e1fa
Archie <57532087+Rockblank@users.noreply.github.com>
2024-11-17 13:36:22 +0700

    LathanRaion17
    kurang skrip enemy jalan sama randomize lokasi

---
+++ Assets/Scripts/RaionLatihanLogixc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaionLatihanLogixc : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 8f;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementVert = Input.GetAxisRaw("Vertical");
+        float movementHori = Input.GetAxisRaw("Horizontal");
+        Vector3 movemenntDirection = new Vector3();
+        movemenntDirection.z = movementVert;
+        movemenntDirection.x = movementHori;
+
+        transform.position += movemenntDirection * movementSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject spawnBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            Destroy(spawnBullet, 1.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/managerGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class managerGame : MonoBehaviour
+{
+    public GameObject enemy;
+    public float intervalTime = 8f;
+    public float currentInterval = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentInterval += Time.deltaTime;
+        if (currentInterval > intervalTime)
+        {
+            currentInterval = 0;
+            GameObject spawningEnemy = Instantiate(enemy,transform.position,Quaternion.identity);
+        }
+    }
+}
