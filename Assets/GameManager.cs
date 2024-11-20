91eb1173d9e0f7ec012a68806f11d81c9dc15cbc
Archie <57532087+Rockblank@users.noreply.github.com>
2024-11-20 11:08:35 +0700

    dikitt lagi
    jkjhgh

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,14 +11,18 @@ public class GameManager : MonoBehaviour
     public float max;
     public float intervalTime = 8f;
     [SerializeField]private float currentTime = 0f;
+    public TMP_Text scoreUI;
+    public TMP_Text highScoreUI;
 
     public TMP_Text textScore;
     public TMP_Text textMiss;
     float score = 0;
+    float highScore = 0;
     float missing = 0;
 
     public GameObject gameOverUI;
     public GameObject scoreAndMiss;
+    public GameObject scoringUI;
     public GameObject winUI;
     // Update is called once per frame
     void Update()
@@ -31,7 +35,11 @@ public class GameManager : MonoBehaviour
             float randomRange = Random.Range(min,max);
             Vector3 newPosition = new Vector3(randomRange, transform.position.y, transform.position.z);
             
-            GameObject spawnEnemy = Instantiate(enemy,newPosition,Quaternion.identity);
+            GameObject spawnEnemy = Instantiate(enemy,newPosition,Quaternion.Euler(90,0,0));
+        }
+        if (score > highScore)
+        {
+            highScore = score;
         }
     }
 
@@ -51,10 +59,19 @@ public class GameManager : MonoBehaviour
     {
         gameOverUI.SetActive(true);
         scoreAndMiss.SetActive(false);
+        ScoringUI();
     }
     public void WinUI()
     {
         winUI.SetActive(true);
         scoreAndMiss.SetActive(false);
+        ScoringUI();
+    }
+
+    public void ScoringUI()
+    {
+        scoringUI.SetActive(true);
+        scoreUI.text = "your score: "+score;
+        highScoreUI.text = "High score: "+highScore;
     }
 }
